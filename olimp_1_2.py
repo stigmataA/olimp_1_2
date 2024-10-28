a17a7453379f624f787e1244b6e512e5b645ca95
stigmataA <r.muhutdinov@gmail.com>
2024-10-28 17:21:25 +0300

    Add files via upload

--- olimp_1_2.py
+++ olimp_1_2.py
@@ -1,31 +1,25 @@
 N = int(input())
 
-# Initialize sums for even and odd numbers
 sum_1 = 0
 sum_2 = 0
 
-# Process each number
 for _ in range(N):
     num = int(input())
     if num % 3 == 0:
-        sum_1 += num  # Add to even sum
+        sum_1 += num
     else:
-        sum_2 += num   # Add to odd sum
+        sum_2 += num
 
-# Process the even sum
 if sum_1 > sum_2:
-    sum_1 *= 2  # Double if divisible by 3
+    sum_1 *= 2
 else:
-    sum_1 *= 3  # Triple if not divisible by 3
+    sum_1 *= 3
 
-# Process the odd sum
 if sum_2 > sum_1:
-    sum_2 *= 2  # Double if divisible by 3
+    sum_2 *= 2
 else:
-    sum_2 *= 3  # Triple if not divisible by 3
+    sum_2 *= 3
 
-# Calculate the final result
 result = sum_1 + sum_2
 
-# Output the result
 print(result)
